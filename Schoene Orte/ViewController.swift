f5617faf2835f94892f9c2bb58b0e858cf21ffd7
Markus Flicker <markusflicker@2a02-8388-e202-5300-7c79-d0f7-7913-f888.cable.dynamic.v6.surfer.at>
2018-11-07 19:04:59 +0100

    Initial Commit

---
+++ Schoene Orte/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schoene Orte
+//
+//  Created by Markus Flicker on 07.11.18.
+//  Copyright © 2018 Worldcare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
